47caf1a2352dface4d48baf3125c89d53851eae9
FunnyVazoniaina <funnyvazoniaina@gmail.com>
2026-02-03 12:50:00 +0300

    fix: changed icon to avoid not displaying

--- food-suggester-frontend/vite.config.ts
+++ food-suggester-frontend/vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "kitchen.png",
+            src: "favicon.png",
             sizes: "192x192",
             type: "image/png",
           },
